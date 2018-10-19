b2aa8bd3a78414181aa9039728096f4c53f3c58d
Nick Dumas <nicholas.dumas@homes.com>
2018-10-19 14:35:23 +0000

    Big commit for issue #2, converting to Cobra.
    This is a relatively straight forward transliteration. I did take the
    liberty of changing version and export into *commands*, rather than
    flags, as that's a little more idiomatic in cobra.

--- cmd/flaredns/main.go
+++ cmd/flaredns/main.go
@@ -3,85 +3,46 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"math/rand"
-	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/vlct-io/pkg/logger"
 
 	"github.com/gobuffalo/envy"
-	"github.com/lfaoro/flares/internal/export"
+	"github.com/spf13/cobra"
 )
 
 var (
-	log         = logger.New()
-	start       = time.Now().UTC().Format(time.RFC3339)
-	exportFlag  = flag.String("export", "", "Path where to export the DNS table files.")
-	versionFlag = flag.Bool("version", false, "Program semantic version.")
+	log   = logger.New()
+	start = time.Now().UTC().Format(time.RFC3339)
 )
 
 // Environment variables
 var (
+	exportFlag  string
 	envErr      error
 	cfAuthKey   string
 	cfAuthEmail string
 )
 
 func init() {
-	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
+
 	envy.Load()
 	cfAuthKey, envErr = envy.MustGet("CF_AUTH_KEY")
 	cfAuthEmail, envErr = envy.MustGet("CF_AUTH_EMAIL")
+
+	cobra.OnInitialize(initConfig)
+	RootCmd.AddCommand(VersionCmd)
+	RootCmd.AddCommand(ExportCmd)
+	return
 }
 
 func main() {
-	if *versionFlag {
-		fmt.Printf("flaredns | %s\n", Version)
-		os.Exit(0)
-	}
-	domains := flag.Args()
-	if len(domains) == 0 {
-		fmt.Println(`Error: no domains provided`)
-		flag.Usage()
-		os.Exit(1)
-	}
 
-	cf := export.Cloudflare{
-		API:       "https://api.cloudflare.com/client/v4",
-		AuthKey:   cfAuthKey,
-		AuthEmail: cfAuthEmail,
-		Client: http.Client{
-			Timeout: time.Second * 10,
-		},
-	}
-
-	if exportFlag != nil {
-		filesDir := filepath.Dir(*exportFlag)
-		if err := os.MkdirAll(filesDir, 0750); err != nil {
-			log.FatalIfErr(err)
-		}
-		for _, domain := range domains {
-			table, err := cf.ExportDNS(domain)
-			log.LogIfErr(err)
-			domain = strings.Replace(domain, ".", "_", -1)
-			filePath := filepath.Join(filesDir, domain+".bind")
-			writeFile(table, filePath)
-			fmt.Println("Exported:", filePath)
-		}
-		return
-	}
-
-	for _, domain := range domains {
-		b, err := cf.ExportDNS(domain)
-		log.LogIfErr(err)
-		fmt.Fprintf(os.Stdout, string(b))
-	}
+	// This should be all that's needed after everything gets converted.
+	Execute()
 }
 
 func writeFile(data []byte, filePath string) error {
