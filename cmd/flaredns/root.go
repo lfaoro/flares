ef7bc0cc9df6b79c8ca6048196524131cae3ad69
Leonardo <leo@vlct.io>
2018-10-30 13:28:19 +0100

    improve CLI documentation
    Signed-off-by: Leonardo <leo@vlct.io>

--- cmd/flaredns/root.go
+++ cmd/flaredns/root.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/lfaoro/flares/internal/cloud"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -22,15 +23,21 @@ var (
 )
 
 var rootCmd = &cobra.Command{
-	Use:     "flaredns",
-	Short:   "flaredns is a CloudFlare DNS backup tool.",
-	Version: Version,
+	Use:          "flaredns [DOMAIN]",
+	Short:        "flaredns is a CloudFlare DNS backup tool.",
+	RunE:         flaredns,
+	SilenceUsage: true,
+	Version:      Version,
+	Example:      "flaredns google.com --export /tmp/dnsdump",
 }
 
 func init() {
 	cobra.OnInitialize(initClient)
 
-	rootCmd.RunE = flaredns
+	rootCmd.SetFlagErrorFunc(func(c *cobra.Command, err error) error {
+		return errors.Wrap(err, "🔥")
+	})
+
 	rootCmd.Flags().BoolVarP(&allFlag, "all", "a", false, "retrieve DNS records table for all domains.")
 	rootCmd.Flags().StringVarP(&exportFlag, "export", "e", "", "export the DNS table into BIND formatted files.")
 	rootCmd.Flags().StringVarP(&keyFlag, "key", "k", "", "CloudFlare API key (defaults to $CF_API_KEY)")
@@ -49,13 +56,13 @@ func flaredns(cmd *cobra.Command, args []string) error {
 	if allFlag {
 		zones, err := client.AllZones()
 		if err != nil {
-			return err
+			return errors.Wrap(err, "🔥")
 		}
 		for _, zone := range zones {
 			split := strings.Split(zone, ",")[1]
 			table, err := client.DNSTableFor(split)
 			if err != nil {
-				return err
+				return errors.Wrap(err, "🔥")
 			}
 			fmt.Fprintf(os.Stdout, string(table))
 		}
@@ -66,19 +73,19 @@ func flaredns(cmd *cobra.Command, args []string) error {
 		for _, domain := range args {
 			table, err := client.DNSTableFor(domain)
 			if err != nil {
-				return err
+				return errors.Wrap(err, "🔥")
 			}
 			domain = strings.Replace(domain, ".", "_", -1)
 			fullDir, err := filepath.Abs(dir)
 			if err != nil {
-				return err
+				return errors.Wrap(err, "🔥")
 			}
 			if err := os.MkdirAll(fullDir, 0755); err != nil {
-				return err
+				return errors.Wrap(err, "🔥")
 			}
 			filePath := filepath.Join(fullDir, domain+".bind")
 			writeFile(table, filePath)
-			fmt.Println("Exported:", filePath)
+			fmt.Println("🔥  Exported:", filePath)
 		}
 		return nil
 	}
@@ -88,7 +95,7 @@ func flaredns(cmd *cobra.Command, args []string) error {
 	for _, domain := range args {
 		b, err := client.DNSTableFor(domain)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "🔥")
 		}
 		fmt.Fprintf(os.Stdout, string(b))
 	}
@@ -113,12 +120,12 @@ func initClient() {
 func writeFile(data []byte, filePath string) error {
 	file, err := os.Create(filePath)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "🔥")
 	}
 	defer file.Close()
 	_, err = file.Write(data)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "🔥")
 	}
 	return nil
 }
