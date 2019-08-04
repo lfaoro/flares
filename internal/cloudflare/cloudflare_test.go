78461ee78d86dce016f23c7fab38a29e0c068dca
Alexandre Guimarães Malucelli <alexandre@malucelli.net>
2019-08-04 14:58:09 -0300

    fix: increase http timeout to cloudflare client

--- internal/cloudflare/cloudflare_test.go
+++ internal/cloudflare/cloudflare_test.go
@@ -8,11 +8,58 @@ package cloudflare
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestNew(t *testing.T) {
+	type args struct {
+		apiKey   string
+		apiEmail string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Cloudflare
+	}{
+		{
+			"NewCloudFlareClient",
+			args{
+				"key",
+				"email",
+			},
+			Cloudflare{
+				API:       "https://api.cloudflare.com/client/v4",
+				AuthKey:   "key",
+				AuthEmail: "email",
+				Client: http.Client{
+					Timeout: time.Second * 30,
+				},
+			},
+		},
+	}
+	t.Run("NewClientApiEmpty", func(t *testing.T) {
+		assert.PanicsWithValue(t, "cloudflare: missing required AuthKey, AuthEmail", func() { New("", "email") })
+	})
+	t.Run("NewClientEmailEmpty", func(t *testing.T) {
+		assert.PanicsWithValue(t, "cloudflare: missing required AuthKey, AuthEmail", func() { New("api", "") })
+	})
+	t.Run("NewClientWithApiAndEmailEmpty", func(t *testing.T) {
+		assert.PanicsWithValue(t, "cloudflare: missing required AuthKey, AuthEmail", func() { New("", "") })
+	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(tt.args.apiKey, tt.args.apiEmail); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func TestCloudflare_Zones(t *testing.T) {
 
